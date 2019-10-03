2b0cfb9cd6edb099b89abe82a22b36b2ee802315
Shubham Rattra <46563834+ShubhamRattra@users.noreply.github.com>
2019-10-03 23:30:44 +0530

    Add files via upload

---
+++ Turbo C++/Difference_of_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+#include <conio.h>
+
+void main()
+{
+	int a, b, c;
+	cout <<"Enter two integers to subtract\n";
+	cin >> a >> b;
+	c = a - b;
+
+	cout <<"Difference of the numbers: " << c << endl;
+	return 0;
+}
